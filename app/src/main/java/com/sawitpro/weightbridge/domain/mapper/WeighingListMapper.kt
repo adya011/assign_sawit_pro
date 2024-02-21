ce8bcf7806d9992e883548585c2dc2063d85ea5f
nandasamudera <samudera.nanda@gmail.com>
2024-02-21 10:40:41 +0700

    feat: rename TruckDataEntity on mapper

--- app/src/main/java/com/sawitpro/weightbridge/domain/mapper/WeighingListMapper.kt
+++ app/src/main/java/com/sawitpro/weightbridge/domain/mapper/WeighingListMapper.kt
@@ -1,17 +1,21 @@
 package com.sawitpro.weightbridge.domain.mapper
 
-import com.sawitpro.weightbridge.data.model.dto.TruckDataDto
-import com.sawitpro.weightbridge.data.model.entity.TruckDataEntity
+import com.sawitpro.weightbridge.data.model.dto.WeighingTicketDto
+import com.sawitpro.weightbridge.data.model.entity.WeighingTicketEntity
 import com.sawitpro.weightbridge.domain.repository.core.DataMapper
+import com.sawitpro.weightbridge.util.orZero
 
-class WeighingListMapper : DataMapper<Map<String, TruckDataDto>, List<TruckDataEntity>> {
-    override fun mapDataModel(dataModel: Map<String, TruckDataDto>?): List<TruckDataEntity> =
-        dataModel?.values?.map {
-            TruckDataEntity(
-                id = it.id,
-                date = it.date,
-                driverName = it.driverName,
-                licenseNumber = it.licenseNumber
+class WeighingListMapper : DataMapper<Map<String, WeighingTicketDto>, List<WeighingTicketEntity>> {
+    override fun mapDataModel(dataModel: Map<String, WeighingTicketDto>?): List<WeighingTicketEntity> =
+        dataModel?.map {
+            WeighingTicketEntity(
+                uId = it.key,
+                date = it.value.date.orEmpty(),
+                driverName = it.value.driverName.orEmpty(),
+                licenseNumber = it.value.licenseNumber.orEmpty(),
+                inboundWeight = it.value.inboundWeight.orZero(),
+                outboundWeight = it.value.outboundWeight.orZero(),
+                netWeight = it.value.inboundWeight.orZero() - it.value.outboundWeight.orZero()
             )
         } ?: emptyList()
 }
