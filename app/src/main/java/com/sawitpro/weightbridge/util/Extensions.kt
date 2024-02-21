457463fda781765d0aa4ffc46e838c5acb6ff107
nandasamudera <samudera.nanda@gmail.com>
2024-02-21 15:35:15 +0700

    feat: extensions for view visibility

--- app/src/main/java/com/sawitpro/weightbridge/util/Extensions.kt
+++ app/src/main/java/com/sawitpro/weightbridge/util/Extensions.kt
@@ -1,3 +1,13 @@
 package com.sawitpro.weightbridge.util
 
+import android.view.View
+
 fun Int?.orZero() = this ?: 0
+
+fun View.setGone() {
+    this.visibility = View.GONE
+}
+
+fun View.setVisible() {
+    this.visibility = View.VISIBLE
+}
