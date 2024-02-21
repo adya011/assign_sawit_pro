1a98d51cab11286a78a753f7394e80e3e3d406bb
nandasamudera <samudera.nanda@gmail.com>
2024-02-21 10:43:23 +0700

    fix: nullable

--- app/src/main/java/com/sawitpro/weightbridge/ui/feature/list/WeighingListViewModel.kt
+++ app/src/main/java/com/sawitpro/weightbridge/ui/feature/list/WeighingListViewModel.kt
@@ -1,12 +1,11 @@
 package com.sawitpro.weightbridge.ui.feature.list
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sawitpro.weightbridge.data.core.DataResult
-import com.sawitpro.weightbridge.data.model.entity.TruckDataEntity
+import com.sawitpro.weightbridge.data.model.entity.WeighingTicketEntity
 import com.sawitpro.weightbridge.domain.repository.WeighBridgetRepository
 import com.sawitpro.weightbridge.util.Constant.CHILD_INDEX_ERROR
 import com.sawitpro.weightbridge.util.Constant.CHILD_INDEX_LOADING
@@ -23,8 +22,8 @@ class WeighingListViewModel(
     private val _displayStateLiveData: MutableLiveData<Int> = MutableLiveData()
     val displayStateLiveData: LiveData<Int> get() = _displayStateLiveData
 
-    private val _errorMessageLiveData: MutableLiveData<String> = MutableLiveData()
-    val errorMessageLiveData: LiveData<String> get() = _errorMessageLiveData
+    private val _errorMessageLiveData: MutableLiveData<String?> = MutableLiveData()
+    val errorMessageLiveData: LiveData<String?> get() = _errorMessageLiveData
 
     fun fetchWeighingList() {
         viewModelScope.launch {
