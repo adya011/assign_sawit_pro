d707112bd1432536ce5e6bfa4dd71ece418b37fc
nandasamudera <samudera.nanda@gmail.com>
2024-02-21 15:35:27 +0700

    feat: BaseFragment

--- app/src/main/java/com/sawitpro/weightbridge/ui/feature/base/BaseFragment.kt
+++ app/src/main/java/com/sawitpro/weightbridge/ui/feature/base/BaseFragment.kt
@@ -1,4 +1,26 @@
 package com.sawitpro.weightbridge.ui.feature.base
 
-class BaseFragment {
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+abstract class BaseFragment : Fragment() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activity?.onBackPressedDispatcher?.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    findNavController().navigateUp()
+                }
+            }
+        )
+    }
+
+    fun showToast(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
 }
