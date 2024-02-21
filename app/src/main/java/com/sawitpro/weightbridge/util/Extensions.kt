5d0311fb251fbaabdd685d37290f2673895de884
nandasamudera <samudera.nanda@gmail.com>
2024-02-21 18:04:09 +0700

    feat: extension for editable to int

--- app/src/main/java/com/sawitpro/weightbridge/util/Extensions.kt
+++ app/src/main/java/com/sawitpro/weightbridge/util/Extensions.kt
@@ -1,8 +1,18 @@
 package com.sawitpro.weightbridge.util
 
+import android.text.Editable
 import android.view.View
 
 fun Int?.orZero() = this ?: 0
+
+fun Editable?.toInt(): Int {
+    val str = this.toString()
+    val int: Int = if (str.isEmpty() || str == "null") {
+        0
+    } else str.toInt()
+
+    return int
+}
 
 fun View.setGone() {
     this.visibility = View.GONE
