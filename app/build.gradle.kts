8886d0ffef0b77b0d7246ef3460a46ea956aac00
nandasamudera <samudera.nanda@gmail.com>
2024-02-22 13:46:25 +0700

    feat: jvm target to 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,11 +35,11 @@ android {
         dataBinding = true
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
@@ -49,6 +49,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // UI
+    implementation(AndroidLibraries.swipeRefresh)
 
     // Dependency Injection
     implementation(AndroidLibraries.koin)
